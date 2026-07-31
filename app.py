216f5399b1b1ea9e258d0da6f6bd3e977ee84cfd
KSuren70 <surender.kalavena@gmail.com>
2026-07-31 13:59:38 -0400

    Remove print statement from sms function
    Removed print statement for latest SMS in sms function.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-AUTHORIZED_NUMBER = "+12362055011"   # your SMS sender number
+AUTHORIZED_NUMBER = "+12362055011"
 latest_sms = None
 
 @app.route('/sms', methods=['POST'])
@@ -10,12 +10,10 @@ def sms():
     global latest_sms
     data = request.json
 
-    # Only accept SMS from one number
     if data.get("from") != AUTHORIZED_NUMBER:
         return {"status": "ignored"}, 403
 
     latest_sms = data
-    print("Latest SMS:", latest_sms)
     return {"status": "ok"}
 
 @app.route('/requests', methods=['GET'])
