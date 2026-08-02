360879105189c89955eb4850f6d9b785b865561f
KSuren70 <surender.kalavena@gmail.com>
2026-08-01 23:31:22 -0400

    Update AUTHORIZED_NUMBER format in app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 # Your real phone number (the one that sends OTP)
-AUTHORIZED_NUMBER = "AOL"
+AUTHORIZED_NUMBER = "+12362055011"
 
 latest_sms = None
 
